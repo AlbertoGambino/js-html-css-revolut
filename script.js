b504202ab81b5fc9aa7cee46cdff5393e76aa429
Alberto Gambino <gambafb@libero.it>
2021-03-23 18:08:00 +0100

    dropdown flex

--- script.js
+++ script.js
@@ -3,11 +3,9 @@
   function init() {
 
   $('.nav-right span').hover(function(){
-    // prendo tutti elementi dropdown active
-    // rimuovo active
 
     $(this).next().addClass('active')
-    // $('.dropdown').addClass('active')
+
   }, function(){
 
     $('.dropdown').removeClass('active')})
