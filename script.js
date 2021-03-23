0d617bfb09d628c1bf72cc32e0c572ea4ef539ad
Alberto Gambino <gambafb@libero.it>
2021-03-23 17:46:56 +0100

    Esercizio base

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function init() {
+
 
   function init() {
 
   $('.nav-right span').hover(function(){
+    // prendo tutti elementi dropdown active
+    // rimuovo active
 
-    $(this).find('.dropdown').addClass('active')
+    $(this).next().addClass('active')
     // $('.dropdown').addClass('active')
   }, function(){
-    
+
     $('.dropdown').removeClass('active')})
 }
+
 $(document).ready(init)
