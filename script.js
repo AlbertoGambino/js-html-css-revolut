e066a31c4a534d513b6090d39688b69c7a4a45ef
Alberto Gambino <gambafb@libero.it>
2021-03-23 15:27:21 +0100

    prova hover

---
+++ script.js
@@ -0,0 +1,13 @@
+function init() {
+
+  function init() {
+
+  $('.nav-left').hover(function(){
+
+      console.log('prova1')
+
+  }, function(){
+
+      console.log('prova2')})
+}
+$(document).ready(init)
